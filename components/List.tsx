f8ac34731a933bac351a5959573e97b727d44c0a
Michallesss <mwiemszra@gmail.com>
2024-03-17 17:50:29 +0100

    ✨ Add list component

---
+++ components/List.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import usePokemon from "@/functions/usePokemon";
+
+export default async function List() {
+  const [pokemons, error] = await usePokemon();
+
+  if (error) return <h1>Error: {error.message}</h1>;
+
+  return (
+      <ul className="grid grid-cols-10 gap-4">
+        {pokemons.map((pokemon, index) => 
+          <Link key={index} href={`/${pokemon.name}`} className="hover:underline">
+            <li>
+              <Image
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
+                alt={pokemon.name}
+                width={100}
+                height={100}
+              />
+              {pokemon.name}
+            </li>
+          </Link>
+        )}
+      </ul>
+  );
+}
