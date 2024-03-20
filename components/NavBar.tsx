d3e327eb1958851d345e1dd0ac6193a790593354
Mikeal <77624159+Michallesss@users.noreply.github.com>
2024-03-20 08:36:10 +0100

    🚑️ Critical pico uifix

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useRouter } from 'next/navigation'
 import { useState } from 'react';
-// import '@picocss/pico';
+import '@picocss/pico';
 
 export default function NavBar() {
   const router = useRouter();
