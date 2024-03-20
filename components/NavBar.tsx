901d7d4aac1d06a63c25b78bf434339762fced51
Mikeal <77624159+Michallesss@users.noreply.github.com>
2024-03-20 08:35:24 +0100

    ✨ Add random poke button

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -12,10 +12,16 @@ export default function NavBar() {
     router.push(`/${search}`);
   }
 
+  const handleRandom = (e: any) => {
+    const random: number = Math.floor(Math.random() * 1302);
+    router.push(`/${random}`);
+  }
+
   return (
       <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Search" onChange={(e) => setSearch(e.target.value)} value={search} minLength={3} maxLength={12} pattern="[A-Za-z\-]+" required/>
         <button type='submit' className="text-black">Search</button>
+        <button type='button' onClick={handleRandom} className="text-black">Random</button>
       </form>
   );
 }
